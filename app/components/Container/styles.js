c17667b2fb1a0dfd66277b3226eb85827d1e9181
Wilmerson da Silva <wilmersondasilva@gmail.com>
2019-03-22 01:29:45 -0300

    Create a home screen

---
+++ app/components/Container/styles.js
@@ -0,0 +1,11 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+const styles = EStyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '$primaryBlue',
+  },
+});
+
+export default styles;
