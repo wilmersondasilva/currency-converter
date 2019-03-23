75dd76d67f5b1cb16d605c139d2519e9e2ecd18b
Wilmerson da Silva <wilmersondasilva@gmail.com>
2019-03-23 04:22:41 -0300

    Create ClearButton component

--- app/components/Button/styles.js
+++ app/components/Button/styles.js
@@ -1,7 +1,23 @@
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 const styles = EStyleSheet.create({
-
+  container: {
+    alignItems: 'center',
+  },
+  wrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    width: 19,
+    marginRight: 11,
+  },
+  text: {
+    color: '$white',
+    fontSize: 14,
+    fontWeight: '300',
+    paddingVertical: 20,
+  },
 });
 
 export default styles;
