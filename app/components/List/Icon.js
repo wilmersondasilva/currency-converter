a5f43e02f990a7285e49a61821d72e66baf473b8
Wilmerson da Silva <wilmersondasilva@gmail.com>
2019-03-25 03:29:49 -0300

    Create Themes screen

--- app/components/List/Icon.js
+++ app/components/List/Icon.js
@@ -4,16 +4,26 @@ import { View, Image } from 'react-native';
 
 import styles from './styles';
 
-const Icon = ({ checkmark, visible }) => {
+const Icon = ({ checkmark, visible, iconBackground }) => {
   const iconStyles = [styles.icon];
 
   if (visible) {
     iconStyles.push(styles.iconVisible);
   }
 
+  if (iconBackground) {
+    iconStyles.push({ backgroundColor: iconBackground });
+  }
+
   return (
     <View style={iconStyles}>
-      {checkmark ? <Image resizeMode="contain" style={styles.checkIcon} source={require('./images/check.png')} /> : null}
+      {checkmark ? (
+        <Image
+          resizeMode="contain"
+          style={styles.checkIcon}
+          source={require('./images/check.png')}
+        />
+      ) : null}
     </View>
   );
 };
@@ -21,6 +31,7 @@ const Icon = ({ checkmark, visible }) => {
 Icon.propTypes = {
   checkmark: PropTypes.bool,
   visible: PropTypes.bool,
+  iconBackground: PropTypes.string,
 };
 
 export default Icon;
