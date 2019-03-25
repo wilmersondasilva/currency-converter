823060dbedea4343f2bd67dbfa5799ca2482e408
Wilmerson da Silva <wilmersondasilva@gmail.com>
2019-03-25 02:54:21 -0300

    Create Options screen

--- app/components/List/ListItem.js
+++ app/components/List/ListItem.js
@@ -6,12 +6,18 @@ import styles from './styles';
 import Icon from './Icon';
 
 const ListItem = ({
-  text, onPress, selected = false, checkmark = true, visible = true,
+  text,
+  onPress,
+  selected = false,
+  checkmark = true,
+  visible = true,
+  customIcon = null,
 }) => (
   <TouchableHighlight onPress={onPress} underlayColor={styles.$underlayColor}>
     <View style={styles.row}>
       <Text style={styles.text}>{text}</Text>
       {selected ? <Icon checkmark={checkmark} visible={visible} /> : <Icon />}
+      {customIcon}
     </View>
   </TouchableHighlight>
 );
@@ -22,6 +28,7 @@ ListItem.propTypes = {
   selected: PropTypes.bool,
   checkmark: PropTypes.bool,
   visible: PropTypes.bool,
+  customIcon: PropTypes.element,
 };
 
 export default ListItem;
---
+++ app/screens/Options.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { ScrollView, StatusBar, Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+import { ListItem, Separator } from '../components/List';
+
+const ICON_COLOR = '#868686';
+const ICON_SIZE = 23;
+const ICON_PREFIX = Platform.OS === 'ios' ? 'ios' : 'md';
+
+class CurrencyList extends Component {
+  handleThemePress = () => {
+    console.log('row press');
+  };
+
+  handleSitePress = () => {
+    console.log('row press');
+  };
+
+  render() {
+    return (
+      <ScrollView>
+        <StatusBar barStyle="default" translucent={false} />
+        <ListItem
+          text="Themes"
+          onPress={this.handleThemePress}
+          customIcon={
+            <Ionicons name={`${ICON_PREFIX}-arrow-forward`} color={ICON_COLOR} size={ICON_SIZE} />
+          }
+        />
+        <Separator />
+        <ListItem
+          text="Fixer.io"
+          onPress={this.handleSitePress}
+          customIcon={<Ionicons name={`${ICON_PREFIX}-link`} color={ICON_COLOR} size={ICON_SIZE} />}
+        />
+      </ScrollView>
+    );
+  }
+}
+
+export default CurrencyList;
