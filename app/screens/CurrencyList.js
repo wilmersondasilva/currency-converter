72b13c4b299753afedfb86b54efacc1e394d6074
Wilmerson da Silva <wilmersondasilva@gmail.com>
2019-03-24 23:58:19 -0300

    Create CurrencyList screen

---
+++ app/screens/CurrencyList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  Text, FlatList, View, StatusBar,
+} from 'react-native';
+
+import currencies from '../data/currencies';
+
+const CurrencyList = () => (
+  <View>
+    <StatusBar barStyle="default" translucent={false} />
+    <FlatList
+      data={currencies}
+      renderItem={({ item }) => <Text>{item}</Text>}
+      keyExtractor={item => item}
+    />
+  </View>
+);
+
+export default CurrencyList;
